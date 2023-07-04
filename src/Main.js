77a886351705d9a72d38101a402d4aeff3181412
Pavel-Kliukin <9213087@mail.ru>
2023-07-05 01:57:08 +0300

    Input form done

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.css';
 
 const Main = () => {
+  const [number, setNumber] = useState(0);
+
+  const numberHandler = (e) => {
+    const { value } = e.target;
+    setNumber(value)
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(number);
+  };
+
   return (
     <div className='Main'>
-      <h1>This is the main part</h1>
+      <h1>Number: {number}</h1>
+      <form onSubmit={submitHandler}>
+        <div className="numberInput">
+          <label htmlFor="number">Enter a positive integer</label>
+          <input
+            type="number"
+            id="number"
+            onChange={numberHandler}
+            required
+          />
+        </div>
+        <button id='submitButton' type="submit">Show Collatz's row</button>
+      </form>
     </div>
   );
 };
