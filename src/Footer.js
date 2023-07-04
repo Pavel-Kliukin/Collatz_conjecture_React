39212aaf712caa4f7a2c4a115bb897a0cb91d572
Pavel-Kliukin <9213087@mail.ru>
2023-07-05 01:24:17 +0300

    Project preparations done

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return <footer className="footer">
+  <div className='footerBorder'></div>
+  <p id="footerText">Copyright © Made by Pavel Kliukin in 2023</p>
+</footer>
+};
+
+export default Footer;
