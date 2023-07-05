5b0829383f253a99a2b351807065df3de75ce493
Pavel-Kliukin <9213087@mail.ru>
2023-07-06 02:48:06 +0300

    Main functionality is done

--- src/Main.js
+++ src/Main.js
@@ -3,30 +3,34 @@ import './Main.css';
 
 const Main = () => {
   const [number, setNumber] = useState(0);
-  const [collatzArray, setcollatzArray] = useState([]);
+  const [isValidNumber, setIsValidNumber] = useState(true);
+  const [collatzArray, setCollatzArray] = useState([]);
 
   const numberHandler = (e) => {
     const { value } = e.target;
-    setNumber(value)
+    setNumber(parseInt(value))
   };
 
   const submitHandler = (e) => {
     e.preventDefault();
+    setIsValidNumber(true)
     collatz_calculations (number)
   };
 
   function collatz_calculations (x) {
-    var tempArray = [parseInt(x)];
-    while (x != 1) {
-      if (x % 2) {
-        x = x*3+1
-        tempArray.push(x)
-      } else {
-        x = x/2
-        tempArray.push(x);
+    if (Number.isInteger(x) && x > 0) {
+      var tempArray = [x];
+      while (x !== 1) {
+        if (x % 2) {
+          x = x*3+1
+          tempArray.push(x)
+        } else {
+          x = x/2
+          tempArray.push(x);
+        }
       }
-    }
-    console.log(tempArray)
+      setCollatzArray(tempArray)
+    } else {setIsValidNumber(false)}
   }
 
   return (
@@ -44,8 +48,19 @@ const Main = () => {
         </div>
         <button id='submitButton' type="submit">Show Collatz's row</button>
       </form>
+      { isValidNumber ? (
+          collatzArray.map( i => {
+            return (
+              <div key={i}>{i}, </div>
+            ); 
+          })
+        ) : (
+          <div>The number you entered is not valid</div>
+        )
+      }
     </div>
   );
+
 };
 
 export default Main;
