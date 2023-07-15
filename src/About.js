824bf7c2bd4d25899a5e83fc366c8cb8c33197ab
Pavel-Kliukin <9213087@mail.ru>
2023-07-15 23:25:17 +0300

    Started About part

--- src/About.js
+++ src/About.js
@@ -4,7 +4,7 @@ import style from './About.module.css';
 const About = () => {
   return (
     <div className={style.About}>
-      
+      <h1 className={style.h1}>About Collatz Conjecture</h1>
     </div>
   );
 };
