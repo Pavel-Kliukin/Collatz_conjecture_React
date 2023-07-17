86a8993d1649280f3b3f3557d9da92d120e82443
Pavel-Kliukin <9213087@mail.ru>
2023-07-18 00:44:15 +0300

    Modal window for logo  added

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,26 @@ import style from './Header.module.css';
 import collatzImage from './assets/collatz_by_K.A.Rousan.webp'
 
 const Header = () => {
+
+  const logoHandler = () => {
+    const modal = document.getElementById(style.modal)
+    modal.classList.remove(style.hide)
+  }
+
+  const closeButtonHandler = () => {
+    const modal = document.getElementById(style.modal)
+    modal.classList.add(style.hide)
+  }
+
   return (
     <div className={style.Header}>
-      <img id={style.collatzImage} className={style.headerImg} src={collatzImage} alt="collatz_by_K.A.Rousan"/>
+      <img onClick={logoHandler} id={style.collatzImage} className={style.headerImg} src={collatzImage} alt="collatz_by_K.A.Rousan"/>
       <h1 className={style.h1}>Collatz Conjecture</h1>
+      <div id={style.modal} className={style.hide}>
+        <button onClick={closeButtonHandler} className={style.closeButton}>X</button>
+        <img className={style.modalImg} src={collatzImage} alt="collatz_by_K.A.Rousan"/>
+        <p>This image is cool</p>
+      </div>
     </div>
   );
 };
